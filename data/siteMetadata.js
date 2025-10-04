898390e5b9ea17409d73d52fdb417a5300dbb898
Victor Morel <victor.m@grafit.pro>
2025-10-04 18:21:51 +0200

    feat: setup

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,14 +2,14 @@
 const siteMetadata = {
   title: 'Yet Another Blog',
   author: 'Victor Morel',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  headerTitle: 'YAB - Yet Another Blog',
+  description: 'This is a blog on random stuff.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   // siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
+  // socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   // mastodon: 'https://mastodon.social/@mastodonuser',
   // email: 'address@yoursite.com',
   // github: 'https://github.com',
