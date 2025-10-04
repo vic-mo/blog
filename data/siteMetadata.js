fe869eadec59ccaa2706c435657ac41f619a2397
Victor Morel <victor.m@grafit.pro>
2025-10-04 22:35:14 +0200

    fix: add back view on github url

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://vicmo.xyz/',
-  // siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/vic-mo/blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/vm.ico`,
   // socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   // mastodon: 'https://mastodon.social/@mastodonuser',
